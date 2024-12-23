07064828da12437d25101a3f998f783525e59936
tanmay5110 <tanmaypatil5110@gmail.com>
2024-12-24 03:40:56 +0530

    Update selection.js

--- Web_Game/js/selection.js
+++ Web_Game/js/selection.js
@@ -3,3 +3,7 @@ document.getElementById("ticTacToeImage").addEventListener("click", function() {
     // Redirect to Tic-Tac-Toe game page
     window.location.href = "tictactoe.html";
 });
+document.getElementById("reactionTestImage").addEventListener("click", function() {
+    // Redirect to Reaction Test game page
+    window.location.href = "reaction_test.html";
+});
